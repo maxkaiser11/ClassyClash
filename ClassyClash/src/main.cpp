15593e5c570db38c69092b058febe0855dc6a8f8
Max Kaiser <max@kaiser.family>
2024-03-17 22:33:29 +0100

    Major updates, cleaned up code by moving Character class into seperate file

--- ClassyClash/src/main.cpp
+++ ClassyClash/src/main.cpp
@@ -1,51 +1,20 @@
 #include "raylib.h"
 #include "raymath.h"
+#include "Character.h"
 
-class Character
-{
-public:
-	Vector2 getWorldPos() { return worldPos; }
-
-private:
-	Texture2D texture;
-	Texture2D idle;
-	Texture2D run;
-	Vector2 screenPos;
-	Vector2 worldPos;
-
-	// Animation variables
-	float rightLeft{ 1.f };
-	float runningTime{};
-	int frame{};
-	const int maxFrames{ 6 };
-	const float updateTime{ 1.f / 12.f };
-};
 
 int main()
 {
-	const int windowWidth{ 960 };
-	const int windowHeight{ 540 };
+	const int windowWidth{ 384 };
+	const int windowHeight{ 384 };
 
 	InitWindow(windowWidth, windowHeight, "Goob's Adventure");
 
 	Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
 	Vector2 mapPos{ 0.0,0.0 };
-	float speed{ 4.0 };
 
-	Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
-	Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
-	Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
-	Vector2 knightPos{
-		(float)windowWidth / 2.0f - 4.0f * (0.5f * (float)knight.width / 0.6f),
-		(float)windowHeight / 2.0f - 4.0f * (0.5f * (float)knight.height)
-	};
-	float rightLeft{ 1.f };
-
-	// Animation variables
-	float runningTime{};
-	int frame{};
-	const int maxFrames{ 6 };
-	const float updateTime{ 1.f / 12.f };
+	Character knight;
+	knight.setScreenPos(windowWidth, windowHeight);
 
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
@@ -53,41 +22,12 @@ int main()
 		BeginDrawing();
 		ClearBackground(WHITE);
 
-		Vector2 direction{};
-		// Movement
-		if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-		if (IsKeyDown(KEY_D)) direction.x += 1.0;
-		if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-		if (IsKeyDown(KEY_S)) direction.y += 1.0;
+		mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
-		if (Vector2Length(direction) != 0.0)
-		{
-			// set mapPos = mapPos - directions
-			mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction),speed));
-			direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-			knight = knight_run;
-		}
-		else
-		{
-			knight = knight_idle;
-		}
 		// Draw the map
 		DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
-		// update animation frame
-		runningTime += GetFrameTime();
-		if (runningTime >= updateTime)
-		{
-			frame++;
-			runningTime = 0.f;
-			if (frame > maxFrames) frame = 0;
-		}
-
-		// Draw the character
-		Rectangle source{frame * (float)knight.width / 6.f, 0.f, rightLeft * (float)knight.width / 6.f, (float)knight.height };
-		Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
-		DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
-
+		knight.tick(GetFrameTime());
 
 		EndDrawing();
 	}
