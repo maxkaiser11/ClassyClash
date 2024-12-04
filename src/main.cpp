fe206eada0c24a25caa08a5f1efae9bbe99d4f9b
crubz11 <max@kaiser.family>
2024-12-04 09:25:41 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main()
+{
+    const int windowWidth = 380;
+    const int windowHeight = 380;
+
+    InitWindow(windowWidth, windowHeight, "Classy Clash");
+
+    while(!WindowShouldClose())
+    {
+        
+    }
+
+    return 0;
+}
