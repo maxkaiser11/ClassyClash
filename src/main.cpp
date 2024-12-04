b628f9af66b397025821e907d2b8841947be5a0f
crubz11 <max@kaiser.family>
2024-12-04 09:57:29 +0100

    added the character

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,54 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
-    const int windowWidth = 380;
-    const int windowHeight = 380;
-
+    const int windowWidth = 384;
+    const int windowHeight = 384;
     InitWindow(windowWidth, windowHeight, "Classy Clash");
 
-    while(!WindowShouldClose())
+    Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 mapPos = {0.0f, 0.0f};
+    float speed = 4.0f;
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos = {
+        (float)windowWidth / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f),
+        (float)windowHeight / 2.0f - 4.0f * (0.5f * (float)knight.height)};
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
     {
-        
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        Vector2 direction = {};
+        if (IsKeyDown(KEY_A))
+            direction.x -= 1.0f;
+        if (IsKeyDown(KEY_D))
+            direction.x += 1.0f;
+        if (IsKeyDown(KEY_W))
+            direction.y -= 1.0f;
+        if (IsKeyDown(KEY_S))
+            direction.y += 1.0f;
+        if (Vector2Length(direction) != 0.0f)
+        {
+
+            // set map pos = mapPos - direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
+        DrawTextureEx(map, mapPos, 0.0f, 4.0f, WHITE);
+
+        // draw the knight
+        Rectangle source = {0.f, 0.f, (float)knight.width / 6.f, (float)knight.height};
+        Rectangle dest = {knightPos.x, knightPos.y, 4.0f * (float)knight.width / 6.0f, 4.0f * (float)knight.height};
+
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
+
+        EndDrawing();
     }
+    CloseWindow();
 
     return 0;
 }
